2ff0b8431de16304365d4578938aa850b456d756
VinceDome <bartha.vince.dome@gmail.com>
2024-04-19 09:10:58 +0200

    finished square detection, started clock application

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ class Button:
         
         color = (255, 0, 0)
         if objects is not None:
-            for ob in objects[0, :]:
+            for ob in objects:#[0, :]:
                 if (self.t[0] < ob[0] < self.b[0]) and (self.t[1] < ob[1] < self.b[1]):
                     color = (0, 0, 255)
 
@@ -63,9 +63,12 @@ class Button:
             elif self.timer + selectionTime < time.perf_counter():
                 self.isPressed = True
                 return
+            
+            ratio = (time.perf_counter()-self.timer)/selectionTime
+            cv2.line(screen, self.t, (int(self.t[0]+(self.b[0]-self.t[0])*ratio), self.t[1]),(0, 255, 0), self.recThick)
         self.isPressed = False
 
-class SliderTest:
+class Slider:
     value = 0
     def __init__(self, t, b):
         self.t = t
@@ -73,10 +76,10 @@ class SliderTest:
     def update(self, screen, objects):
         cv2.line(screen, self.t, self.b, (255, 0, 0), 20)
         if objects is not None:
-            for ob in objects[0, :]:
+            for ob in objects:#[0, :]:
                 if (self.t[0]-100 < ob[0] < self.t[0]+100) and (self.t[1] < ob[1] < self.b[1]):
-                    cv2.line(screen, self.t, (self.t[0], ob[1]), (0, 255, 0), 20)
-                    cv2.circle(screen, (self.t[0], ob[1]), 30, (0, 255, 0), 30)
+                    cv2.line(screen, self.t, (self.t[0], int(ob[1])), (0, 255, 0), 20)
+                    cv2.circle(screen, (self.t[0], int(ob[1])), 30, (0, 255, 0), 30)
                     self.value = (ob[1]-self.t[1])/(self.b[1]-self.t[1])
                     return
         self.value = 0
@@ -97,18 +100,24 @@ def DetectCircles(source):
     # Ellenőrizze, hogy talált-e köröket
     if insideCircles is not None:
         # Körök koordinátáinak kinyerése
-        return np.uint16(np.around(insideCircles))
+        return np.uint16(np.around(insideCircles))[0, :]
 
 def DetectSquares(source, screen):
+    #blurring
     blurred = cv2.GaussianBlur(source, (5, 5), 0)
+    #thresholding
     _, edges = cv2.threshold(blurred, 50, 255, cv2.THRESH_BINARY)
+    #drawing contours
     contours, _ = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
     test = np.zeros((screenSize[1], screenSize[0], 3), dtype = np.uint8)
+    squares = []
     for contour in contours:
     # Approximate the contour to a polygon
         peri = cv2.arcLength(contour, True)
         approx = cv2.approxPolyDP(contour, (0.05-0.03) * peri, True)
 
+        #drawing to test screen
         cv2.drawContours(test, contour, -1, (0, 255, 0), 2)
         cv2.drawContours(test, [approx], -1, (0, 0, 255), 2)
         
@@ -116,22 +125,33 @@ def DetectSquares(source, screen):
         # If the contour has 4 vertices, it's likely a square
         if len(approx) == 4 and peri < 300 and peri > 200:
             cv2.drawContours(screen, [approx], -1, (255, 0, 0), 10)
-            
+            #calculating centre based on average vertice location
+            x=0
+            y=0
+            for i in approx:
+                cv2.circle(screen, i[0], 5, (0, 0, 255), 5)
+                x+=i[0][0]
+                y+=i[0][1]
+            squares.append((x/4, y/4))
 
-        if len(approx) == 3 and peri < 300 and peri > 200:
+        if len(approx) == 3 and peri < 300 and peri > 0:
             cv2.drawContours(screen, [approx], -1, (255, 0, 0), 10)
+
+    #displaying test screens
     cv2.imshow("teszt", edges)
     cv2.imshow("teszt2", test)
+
+    return squares
 
 def DisplayCircles(screen, circles):
     if circles is None:
         return
-    for circle in circles[0, :]:
+    for circle in circles:#[0, :]:
+        
         center = (circle[0], circle[1])
         radius = circle[2]
         cv2.circle(screen, center, radius+5, (0, 0, 255), 3)
         #cv2.circle(screen, center, testradius, (255, 0, 0), testwidth)
-
 
 def text_utf(image, string, loc, size):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -154,33 +174,12 @@ def CenteredText(image, string, loc, size, color, thickness):
     cv2.putText(image, string, loc, cv2.FONT_HERSHEY_COMPLEX, size, color, thickness)
     return image
 
-"""
-def Button(screen, circles, t, b, recThick, string, strSize, strThick):
-    color = (255, 0, 0)
-    if circles is not None:
-        for circle in circles[0, :]:
-            if (t[0] < circle[0] < b[0]) and (t[1] < circle[1] < b[1]):
-                color = (0, 0, 255)
+def RandomHour():
 
-    CenteredText(screen, string, (t[0]+(b[0]-t[0])/2, t[0]+(b[0]-t[0])/2), strSize, color, strThick)
-    cv2.rectangle(screen, t, b, color, recThick)
-
-    if color == (255, 0, 0):
-        return False
-    else:
-        return True
-    
-"""
-
-def Slider(screen, x, y1, y2, circles):
-    cv2.line(screen, (x, y1), (x, y2), (255, 0, 0), 20)
-    if circles is not None:
-        for circle in circles[0, :]:
-            if (x-100 < circle[0] < x+100) and (y1 < circle[1] < y2):
-                cv2.line(screen, (x, y1), (x, circle[1]), (0, 255, 0), 20)
-                cv2.circle(screen, (x, circle[1]), 30, (0, 255, 0), 30)
-                return (circle[1]-y1)/(y2-y1)
-    return 0
+    num = random.randint(0, 288)
+    nagymutato = num % 12
+    kismutato = (num/12) % 12
+    print(num, nagymutato, kismutato)
 
 with open("parameters.txt", "r", encoding="utf-8") as file:
     for i in file:
@@ -188,7 +187,6 @@ with open("parameters.txt", "r", encoding="utf-8") as file:
 
 with open("allocations.txt", "r", encoding="utf-8") as file:
     allLocations = file.read().split("\n")
-
 
 if webcam==0: #integrált cam
     cap = cv2.VideoCapture(webcam)
@@ -209,19 +207,21 @@ elif webcam==2: # OBS
 megyek = cv2.imread("vakterkep_edit.png")
 megyek = cv2.resize(megyek, screenSize)
 
+ora = cv2.imread("cropped-ora.png")
+ora = cv2.resize(ora, screenSize)
 #ablak kreálás
-cv2.namedWindow("Webkamera", cv2.WINDOW_NORMAL)
+cv2.namedWindow("main", cv2.WINDOW_NORMAL)
 
 #az ablaknak a mozgatása a projektor másodlagos képernyőjére
-cv2.moveWindow("Webkamera", yogaSize[0], 0)
+cv2.moveWindow("main", yogaSize[0], 0)
 
 cv2.namedWindow("teszt", cv2.WINDOW_NORMAL)
 cv2.namedWindow("teszt2", cv2.WINDOW_NORMAL)
 cv2.moveWindow("teszt2", 0, 500)
 
 #fulscreen
-cv2.setWindowProperty("Webkamera", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-cv2.imshow("Webkamera", megyek)
+cv2.setWindowProperty("main", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.imshow("main", megyek)
 cv2.waitKey(1)
 
 
@@ -236,7 +236,8 @@ while running:
     chosenLocations = []
     gameButton = Button((500, 600), (1920-500, 900), 10, "Game", 4, 3)
     showButton = Button((500, 200), (1920-500, 500), 10, "Showcase", 4, 3)
-    while not gameButton.isPressed and not showButton.isPressed:
+    clockButton = Button((0, 0), (300, 300), 10, "Clock", 4, 3)
+    while not gameButton.isPressed and not showButton.isPressed and not clockButton.isPressed:
 
         ret, frame = cap.read()
         #check ha hiba
@@ -249,19 +250,20 @@ while running:
         #gray upscale
         gray = cv2.resize(gray, screenSize)
 
-        cv2.imshow('Webkamera', screen)
+        cv2.imshow('main', screen)
         cv2.waitKey(1)
         
         #clear screen
         screen = megyek.copy()
         #screen = np.zeros((screenSize[1], screenSize[0], 3), dtype = np.uint8)
 
-        DetectSquares(gray, screen)
+        squares = DetectSquares(gray, screen)
         
         circles = DetectCircles(gray)
 
         showButton.update(screen, circles)
         gameButton.update(screen, circles)
+        clockButton.update(screen, circles)
 
         DisplayCircles(screen, circles)
 
@@ -270,6 +272,7 @@ while running:
         while game:
             #kérdések kisorsolása egy listába
             allLocationsR = allLocations[:]
+            chosenLocations = []
             for i in range(questions):
                 loc = random.choice(allLocationsR)
                 chosenLocations.append(Location(loc))
@@ -304,11 +307,10 @@ while running:
                     cv2.putText(screen, str(round((st+guessTime-time.perf_counter()))), (100, 300), cv2.FONT_HERSHEY_SIMPLEX, 5, (255, 0, 0), 20)
 
                     circles = DetectCircles(gray)
-                    closest = False
-
+                
                     if circles is not None:
                         #összes körön végigmenni
-                        for circle in circles[0, :]:
+                        for circle in circles:#[0, :]:
                             center = (circle[0], circle[1])
                             radius = circle[2]
 
@@ -316,26 +318,20 @@ while running:
                             if time.perf_counter() > st + guessTime - 0.5:
                                 dist = int(math.dist(location.coords, center))
 
-                                if not closest:
-                                    closest = Guess(dist, center, radius)
-                                elif dist < closest.radius:
-                                    closest = Guess(dist, center, radius)
+                                dists.append(Guess(dist, center, radius))
 
                             cv2.circle(screen, center, radius+5, (0, 0, 255), 3)
                             #cv2.circle(megyek, center, testradius, (255, 0, 0), testwidth)
 
 
+
                     # Kijelzés a képernyőn
 
                     #képfrissítés
-                    cv2.imshow('Webkamera', screen)
+                    cv2.imshow('main', screen)
 
                     cv2.waitKey(1)
 
-                    if closest:
-                        #hozzáadom az iteráció legközelebbi távolságát
-
-                        dists.append(closest)
                 if not dists:
                     best = False
                 else:
@@ -364,7 +360,7 @@ while running:
                     cv2.putText(screen, str(round((wt+displayTime-time.perf_counter()))), (300, 300), cv2.FONT_HERSHEY_SIMPLEX, 5, (255, 0, 0), 20)
 
 
-                    cv2.imshow('Webkamera', screen)
+                    cv2.imshow('main', screen)
                     cv2.waitKey(1)
 
                     time.sleep(0.5)
@@ -386,7 +382,7 @@ while running:
             #cv2.putText(screen, f"Total distance: {totalDistance}", (400,900), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 20)
             CenteredText(screen, f"Total distance: {totalDistance}", (screenSize[0]/2, 950), 3, (255, 0, 0), 10)
             megyekT = screen.copy()
-            cv2.imshow('Webkamera', screen)
+            cv2.imshow('main', screen)
             cv2.waitKey(1)
 
             selecting = True
@@ -411,7 +407,7 @@ while running:
 
                 circles = DetectCircles(gray)
 
-                cv2.imshow('Webkamera', screen)
+                cv2.imshow('main', screen)
                 cv2.waitKey(1)
 
                 screen = megyekT.copy()
@@ -430,8 +426,8 @@ while running:
     
     elif showButton.isPressed:
         showcasing = True
-        menuButton = Button((1600, 700), (1800, 900), 15, "Menu", 4, 3)
-        showSlider = SliderTest((500, 300), (500, 1000))
+        menuButton = Button((1400, 500), (1800, 900), 15, "Menu", 4, 3)
+        showSlider = Slider((500, 300), (500, 1000))
         while showcasing:
             ret, frame = cap.read()
             #check ha hiba
@@ -444,7 +440,7 @@ while running:
             #gray upscale
             gray = cv2.resize(gray, screenSize)
 
-            cv2.imshow('Webkamera', screen)
+            cv2.imshow('main', screen)
             cv2.waitKey(1)
             
             #clear screen
@@ -453,17 +449,75 @@ while running:
 
             circles = DetectCircles(gray)
 
-            DetectSquares(gray, screen)
+            squares = DetectSquares(gray, screen)
 
             DisplayCircles(screen, circles)
 
 
-            showSlider.update(screen, circles)
+            showSlider.update(screen, squares)
+            CenteredText(screen, str(round(showSlider.value, 2)), (900, 750), 5, (255, 0, 0), 4)
             menuButton.update(screen, circles)
-            print(menuButton)
             if menuButton.isPressed:
                 showcasing = False
 
-# Kapcsolat bontása és összes ablak bezárása
+    else:
+
+        while True:
+            
+            st = time.perf_counter()
+            angles = []
+            #! IDEIGLENES
+            correct_angle = 180
+            clock_center = (1000, 1000)
+
+
+            while time.perf_counter() < st + guessTime:
+                # Kép beolvasása a webkamerából
+                
+                ret, frame = cap.read()
+                #check ha hiba
+                if not ret:
+                    print("Hiba a kép beolvasásakor")
+                    break
+
+                #gray
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                #gray upscale
+                gray = cv2.resize(gray, screenSize)
+
+                cv2.imshow('main', screen)
+                cv2.waitKey(1)# Kapcsolat bontása és összes ablak bezárása
+
+                screen = ora.copy()
+
+                circles = DetectCircles(gray)
+
+                if circles is not None:
+                    #összes körön végigmenni
+                    for circle in circles:#[0, :]:
+                        center = (circle[0], circle[1])
+                        radius = circle[2]
+
+                        #utolsó fél másodperc, az összes távolságot elmentem, amiből kiválasztom a legközelebbit
+                        if time.perf_counter() > st + guessTime - 0.5:
+                            
+                            dx, dy = clock_center[0]-center[0], clock_center[1]-center[1]
+                            angle = math.degrees(math.atan2(dy, dx))
+                            dist = abs(correct_angle-angle)
+                            
+
+                            angles.append([angle, dist])
+
+                        cv2.circle(screen, center, radius+5, (0, 0, 255), 3)
+                        #cv2.circle(megyek, center, testradius, (255, 0, 0), testwidth)
+            if not angles:
+                best = False
+            else:
+                best = min(angles, key=lambda x: x[1])
+                best[1] = max(best[1]-5, 0)
+
+
+            playerGuesses.append(best)
+
 cap.release()
 cv2.destroyAllWindows()
